a724edf9cef5ab9cb5a128f850c9ec0ea4164104
mmraihan <mm_raihan@live.com>
2026-06-16 17:48:11 +0600

    CS added

---
+++ dataset-configuration/connectionstring.py
@@ -0,0 +1,6 @@
+{
+  "ConnectionStrings": {
+    "MssqlConnection": "data source=176.9.16.194,1433; initial catalog=JustForRestore-Expense; User Id=sa; Password=Esap.12.Three; MultipleActiveResultSets=True; Application Name=EntityFramework; TrustServerCertificate=True; Encrypt=False",
+    "PostgresConnection": "Host=176.9.16.194;Port=5434;Database=Healthcare-Dataset;Username=postgres;Password=pg1234;Application Name=EntityFramework"
+  }
+}
